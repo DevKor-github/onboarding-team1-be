3b0b1fce8ea4042a3408509a06137ebaf598be79
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-08-07 19:06:25 +0900

    Devkord init

---
+++ src/main/java/me/junbeom/Devkord/DevkordApplication.java
@@ -0,0 +1,11 @@
+package me.junbeom.Devkord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevkordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevkordApplication.class, args);
+    }
+}
